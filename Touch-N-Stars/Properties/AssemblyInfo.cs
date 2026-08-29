481770f03db7dd5d322719f43bbf1bb3071ce38e
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2026-08-29 19:38:23 +0200

    Update assembly version to 1.3.2.0

--- Touch-N-Stars/Properties/AssemblyInfo.cs
+++ Touch-N-Stars/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.CompilerServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Touch 'N' Stars")]
